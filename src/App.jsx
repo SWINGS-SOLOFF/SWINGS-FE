3576ae266fd3108f2fdf4dbf69ecc87e63866864
Gromit-IT <165180593+Gromit-IT@users.noreply.github.com>
2025-03-25 10:35:58 +0900

    새로운 구조 적용 -1

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 // src/App.jsx
-import React from "react";
-import { Outlet } from "react-router-dom";
-import NavBar from "./components/NavBar.jsx";   // 혹은 NavBar.jsx
-import FooterBar from "./1_user/components/FooterBar"; // 경로는 상황에 맞게 조정
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import FooterBar from "./components/FooterBar";
+import UserRoutes from "./1_user/routes/UserRoutes";
 
 export default function App() {
   return (
     <div className="flex flex-col min-h-screen">
-      {/* 상단 공통 네비게이션 */}
       <NavBar />
 
-      {/* 라우터가 렌더링할 각 페이지 */}
-      <main className="flex-grow">
-        <Outlet />
-      </main>
+      <Routes>
+        <Route path="/*" element={<UserRoutes />} />
+      </Routes>
 
-      {/* 하단 공통 푸터 */}
       <FooterBar />
     </div>
   );
