a159da68161b5e1809ae1b2dc76bb3f601aa427f
itroqkfwk <wjdeo3321@gmail.com>
2025-03-25 13:04:08 +0900

    피드및소셜기능 커밋#1
    피드및소셜기능 커밋#1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import GroupPage from './pages/matchgroup/GroupPage.jsx';
-import MatchPage from './pages/match/MatchPage.jsx';
-import MatchDetailPage from './pages/match/MatchDetailPage.jsx';
-import FeedPage from './pages/feed/FeedPage.jsx';
-import LoginPage from './pages/user/LoginPage.jsx';
-import ProfilePage from './pages/user/ProfilePage.jsx';
-import RegisterPage from './pages/user/RegisterPage.jsx';
+// import GroupPage from './pages/matchgroup/GroupPage.jsx';
+// import MatchPage from './pages/match/MatchPage.jsx';
+// import MatchDetailPage from './pages/match/MatchDetailPage.jsx';
+import FeedPage from './2_feed/pages/FeedPage.jsx';
+// import LoginPage from './pages/user/LoginPage.jsx';
+// import ProfilePage from './pages/user/ProfilePage.jsx';
+// import RegisterPage from './pages/user/RegisterPage.jsx';
 import Navigation from './components/Navigation';
+import SocialPage from './2_feed/pages/SocialPage.jsx';
+
 
 function App() {
     return (
@@ -17,15 +19,15 @@ function App() {
                 <div className="container mx-auto px-4 pb-8">
                     <Routes>
                         {/* 기존 HomePage는 없으므로, LoginPage를 홈으로 설정 */}
-                        <Route path="/" element={<LoginPage />} />
-                        <Route path="/user-management" element={<GroupPage />} />
-                        <Route path="/match-management" element={<MatchPage />} />
-                        <Route path="/match-detail" element={<MatchDetailPage />} />
+                        {/*<Route path="/" element={<LoginPage />} />*/}
+                        {/*<Route path="/user-management" element={<GroupPage />} />*/}
+                        {/*<Route path="/match-management" element={<MatchPage />} />*/}
+                        {/*<Route path="/match-detail" element={<MatchDetailPage />} />*/}
                         <Route path="/swings/feed" element={<FeedPage />} />
-                        <Route path="/swings/social" element={<FeedPage />} />
-                        <Route path="/login" element={<LoginPage />} />
-                        <Route path="/profile" element={<ProfilePage />} />
-                        <Route path="/register" element={<RegisterPage />} />
+                        <Route path="/swings/social" element={<SocialPage />} />
+                        {/*<Route path="/login" element={<LoginPage />} />*/}
+                        {/*<Route path="/profile" element={<ProfilePage />} />*/}
+                        {/*<Route path="/register" element={<RegisterPage />} />*/}
                     </Routes>
                 </div>
             </div>
