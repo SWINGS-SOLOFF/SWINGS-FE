b10b3db1ac6f6b980b826a542beed78355f72723
YusungH <hwang5032@gmail.com>
2025-04-01 14:03:40 +0900

    package.json 수정 -1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// src/App.jsx
 import { Routes, Route } from "react-router-dom";
 import UserLayout from "./1_user/layouts/UserLayout";
 import AdminLayout from "./1_user/layouts/AdminLayout";
@@ -6,25 +7,30 @@ import AdminRoutes from "./1_user/routes/AdminRoutes";
 import StartLogin from "./1_user/pages/StartLogin";
 import SignUp from "./1_user/pages/SignUp";
 import MatchRoutes from "./3_match/routes/MatchRoutes";
+import MatchGroupRoutes from "./4_matchgroup/routes/MatchGroupRoutes.jsx";
 
 export default function App() {
-  return (
-    <Routes>
-      {/* ✅ 로그인/회원가입 (Nav 없이) */}
-      <Route path="/swings" element={<StartLogin />} />
-      <Route path="/swings/signup" element={<SignUp />} />
+    return (
+        <Routes>
+            {/* 로그인 / 회원가입 (Nav 없이) */}
+            <Route path="/swings" element={<StartLogin />} />
+            <Route path="/swings/signup" element={<SignUp />} />
 
-      {/* ✅ 관리자 페이지 (AdminNavBar 포함) */}
-      <Route path="/swings/admin/*" element={<AdminLayout />}>
-        <Route path="*" element={<AdminRoutes />} />
-      </Route>
+            {/* 관리자 페이지 (AdminNavBar 포함) */}
+            <Route path="/swings/admin/*" element={<AdminLayout />}>
+                <Route path="*" element={<AdminRoutes />} />
+            </Route>
 
-        
-        <Route path="/swings/match/*" element={<MatchRoutes />} />
+            {/* 매치 페이지 */}
+            <Route path="/swings/match/*" element={<MatchRoutes />} />
 
-      {/* ✅ 사용자 페이지 (NavBar + BottomNavBar 포함) */}
-      <Route path="/swings/*" element={<UserLayout />}>
-        <Route path="*" element={<UserRoutes />} />
-      </Route>
+            {/* 사용자 페이지 (NavBar + BottomNavBar 포함) */}
+            <Route path="/swings/*" element={<UserLayout />}>
+                <Route path="*" element={<UserRoutes />} />
+            </Route>
 
-    </Routes>
+            {/* 매치 그룹 페이지 */}
+            <Route path="/swings/matchgroup/*" element={<MatchGroupRoutes />} />
+        </Routes>
+    );
+}
